7cb1ba4c5f114f5849ffa339e6dd5df92700c915
1745stef <93550368+1745stef@users.noreply.github.com>
2021-11-19 15:41:42 -0500

    Update Client.cs

--- ProjetFinal/ProjetFinal/Client.cs
+++ ProjetFinal/ProjetFinal/Client.cs
@@ -4,9 +4,92 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ProjetFinal
+namespace ProjetFinal 
 {
-    class Client
+    class Client : INotifyPropertyChanged
     {
+        int id;
+        string nom, email, numTel, poste, numBureau, type;
+
+        public Client(int id, string nom, string email, string numTel, string poste, string numBureau, string type)
+        {
+            this.id = id;
+            this.nom = nom;
+            this.email = email;
+            this.numTel = numTel;
+            this.poste = poste;
+            this.numBureau = numBureau;
+            this.type = type;
+        }
+
+        public int Id 
+        { 
+            get => id;
+            set
+            {
+                id = value;
+                this.OnPropertyChanged();
+            }
+        }
+        public string Nom 
+        { 
+            get => nom;
+            set
+            {
+                nom = value;
+                this.OnPropertyChanged();
+            }
+        }
+        public string Email 
+        { 
+            get => email;
+            set
+            {
+                email = value;
+                this.OnPropertyChanged();
+            }
+        }
+        public string NumTel 
+        { 
+            get => numTel;
+            set
+            {
+                numTel = value;
+                this.OnPropertyChanged();
+            }
+        }
+        public string Poste 
+        { 
+            get => poste;
+            set
+            {
+                poste = value;
+                this.OnPropertyChanged();
+            }
+        }
+        public string NumBureau 
+        { 
+            get => numBureau;
+            set
+            {
+                numBureau = value;
+                this.OnPropertyChanged();
+            }
+        }
+        public string Type 
+        { 
+            get => type;
+            set
+            {
+                type = value;
+                this.OnPropertyChanged();
+            }
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName] string propertyName = null) =>
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
     }
 }
