10319d6d54b8e1e0aa5cc6def143b481630689aa
1745stef <93550368+1745stef@users.noreply.github.com>
2021-11-19 15:44:09 -0500

    Update Utilisateur.cs

--- ProjetFinal/ProjetFinal/Utilisateur.cs
+++ ProjetFinal/ProjetFinal/Utilisateur.cs
@@ -6,7 +6,58 @@ using System.Threading.Tasks;
 
 namespace ProjetFinal
 {
-    class Utilisateur
+    class Utilisateur : INotifyPropertyChanged
     {
+                string nomUsager, nom, prenom, password;
+
+        public utilisateur(string nomUsager, string nom, string prenom, string password)
+        {
+            this.nomUsager = nomUsager;
+            this.nom = nom;
+            this.prenom = prenom;
+            this.password = password;
+        }
+
+        public string NomUsager 
+        { 
+            get => nomUsager;
+            set
+            {
+                nomUsager = value;
+                this.OnPropertyChanged();
+            }
+        }
+        public string Nom 
+        { 
+            get => nom;
+            set
+            {
+                nom = value;
+                this.OnPropertyChanged();
+            }
+        }
+        public string Prenom 
+        { 
+            get => prenom;
+            set
+            {
+                prenom = value;
+                this.OnPropertyChanged();
+            }
+        }
+        public string Password 
+        { 
+            get => password;
+            set
+            {
+                password = value;
+                this.OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName] string propertyName = null) =>
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
     }
 }
